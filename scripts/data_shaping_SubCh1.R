4d7d9a21431bec1a061dd96082a739e304d71745
Colby Ford <colbytylerford@gmail.com>
2019-05-31 16:10:38 -0400

    Add data and data prep script for Subchallenge 1

---
+++ scripts/data_shaping_SubCh1.R
@@ -0,0 +1,30 @@
+########################################################
+## Script for reshaping Malaria Dataset               ##
+## For Malaria DREAM Challenge 2019 - Subchallenge 1  ##
+## Written by: Colby T. Ford, Ph.D.                   ##
+########################################################
+
+## Load in packages
+library(dplyr)
+library(tidyr)
+library(readr)
+
+## Load in data
+train <- read_csv("SubCh1_TrainingData.csv")
+
+## Get value column names by which to pivot
+valcols <- colnames(train %>% select(-c(Sample_Name, Isolate, Timepoint, Treatment, BioRep, DHA_IC50)))
+
+## Reshape data: Gets 1 row per isolate, extends each Timepoint, Treatment, and BioRep version as its own column
+train.reshaped <- train %>% 
+  select(-c(Sample_Name, DHA_IC50)) %>%
+  gather(variable, value, valcols) %>% 
+  unite(variable, Timepoint, Treatment, BioRep, variable) %>% 
+  spread(variable, value)
+
+## Add back in the Depedent Variable
+train.reshaped$DHA_IC50 <- train %>% select(c(Isolate, DHA_IC50)) %>% unique()
+
+## Write out dataset
+write_csv(train.reshaped, "SubCh1_TrainingData_Reshaped.csv")
+
