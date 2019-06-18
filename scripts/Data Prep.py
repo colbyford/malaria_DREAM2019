c73ade2092a32040647232b1948bf549d9ad4dde
Colby Ford <colbytylerford@gmail.com>
2019-06-18 17:15:43 -0400

    Add pivot shaping by slice

--- scripts/Data Prep.py
+++ scripts/Data Prep.py
@@ -51,6 +51,174 @@ display(train)
 
 # COMMAND ----------
 
+# column_list = train.columns
+# prefix = "hr24_trDHA_br1_"
+# # new_column_list = [prefix + s for s in column_list]
+# new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+ 
+# column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+# print(column_mapping)
+
+# # train2 = train.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+# # display(train2)
+
+# COMMAND ----------
+
+# DBTITLE 1,Reshape Data
+from pyspark.sql.functions import first, col
+
+## Separate Dependent Variable
+y = train.select(col("Isolate"),
+                 col("DHA_IC50")) \
+         .distinct()
+
+############################################################################
+print("1. Create Slice [Timepoint: 24HR, Treatment: DHA, BioRep: BRep1]")
+hr24_trDHA_br1 = train.drop("Sample_Name","DHA_IC50") \
+                      .filter((col("Timepoint") == "24HR") &
+                              (col("Treatment") == "DHA") &
+                              (col("BioRep") == "BRep1"))
+## Rename Columns
+column_list = hr24_trDHA_br1.columns
+prefix = "hr24_trDHA_br1_"
+new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+
+column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+hr24_trDHA_br1 = hr24_trDHA_br1.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+############################################################################
+
+############################################################################
+print("2. Create Slice [Timepoint: 24HR, Treatment: DHA, BioRep: BRep2]")
+hr24_trDHA_br2 = train.drop("Sample_Name","DHA_IC50") \
+                      .filter((col("Timepoint") == "24HR") &
+                              (col("Treatment") == "DHA") &
+                              (col("BioRep") == "BRep2"))
+## Rename Columns
+column_list = hr24_trDHA_br2.columns
+prefix = "hr24_trDHA_br2_"
+new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+
+column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+hr24_trDHA_br2 = hr24_trDHA_br2.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+############################################################################
+
+############################################################################
+print("3. Create Slice [Timepoint: 24HR, Treatment: UT, BioRep: BRep1]")
+hr24_trUT_br1 = train.drop("Sample_Name","DHA_IC50") \
+                      .filter((col("Timepoint") == "24HR") &
+                              (col("Treatment") == "UT") &
+                              (col("BioRep") == "BRep1"))
+## Rename Columns
+column_list = hr24_trUT_br1.columns
+prefix = "hr24_trUT_br1_"
+new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+
+column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+hr24_trUT_br1 = hr24_trUT_br1.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+############################################################################
+
+############################################################################
+print("4. Create Slice [Timepoint: 24HR, Treatment: UT, BioRep: BRep2]")
+hr24_trUT_br2 = train.drop("Sample_Name","DHA_IC50") \
+                      .filter((col("Timepoint") == "24HR") &
+                              (col("Treatment") == "UT") &
+                              (col("BioRep") == "BRep2"))
+## Rename Columns
+column_list = hr24_trUT_br2.columns
+prefix = "hr24_trUT_br2_"
+new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+
+column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+hr24_trUT_br2 = hr24_trUT_br2.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+############################################################################
+
+############################################################################
+print("5. Create Slice [Timepoint: 6HR, Treatment: DHA, BioRep: BRep1]")
+hr6_trDHA_br1 = train.drop("Sample_Name","DHA_IC50") \
+                      .filter((col("Timepoint") == "6HR") &
+                              (col("Treatment") == "DHA") &
+                              (col("BioRep") == "BRep1"))
+## Rename Columns
+column_list = hr6_trDHA_br1.columns
+prefix = "hr6_trDHA_br1_"
+new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+
+column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+hr6_trDHA_br1 = hr6_trDHA_br1.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+############################################################################
+
+############################################################################
+print("6. Create Slice [Timepoint: 6HR, Treatment: DHA, BioRep: BRep2]")
+hr6_trDHA_br2 = train.drop("Sample_Name","DHA_IC50") \
+                      .filter((col("Timepoint") == "6HR") &
+                              (col("Treatment") == "DHA") &
+                              (col("BioRep") == "BRep2"))
+## Rename Columns
+column_list = hr6_trDHA_br2.columns
+prefix = "hr6_trDHA_br2_"
+new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+
+column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+hr6_trDHA_br2 = hr6_trDHA_br2.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+############################################################################
+
+############################################################################
+print("7. Create Slice [Timepoint: 6HR, Treatment: UT, BioRep: BRep1]")
+hr6_trUT_br1 = train.drop("Sample_Name","DHA_IC50") \
+                      .filter((col("Timepoint") == "6HR") &
+                              (col("Treatment") == "UT") &
+                              (col("BioRep") == "BRep1"))
+## Rename Columns
+column_list = hr6_trUT_br1.columns
+prefix = "hr6_trUT_br1_"
+new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+
+column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+hr6_trUT_br1 = hr6_trUT_br1.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+############################################################################
+
+############################################################################
+print("8. Create Slice [Timepoint: 6HR, Treatment: UT, BioRep: BRep2]")
+hr6_trUT_br2 = train.drop("Sample_Name","DHA_IC50") \
+                      .filter((col("Timepoint") == "6HR") &
+                              (col("Treatment") == "UT") &
+                              (col("BioRep") == "BRep2"))
+## Rename Columns
+column_list = hr6_trUT_br2.columns
+prefix = "hr6_trUT_br2_"
+new_column_list = [prefix + s if s != "Isolate" else s for s in column_list]
+
+column_mapping = [[o, n] for o, n in zip(column_list, new_column_list)]
+
+hr6_trUT_br2 = hr6_trUT_br2.select(list(map(lambda old, new: col(old).alias(new),*zip(*column_mapping))))
+############################################################################
+
+## Join Slices Together
+print("Joining all together...")
+shaped_train = y.join(hr24_trDHA_br1, "Isolate") \
+                .join(hr24_trDHA_br2, "Isolate") \
+                .join(hr24_trUT_br1, "Isolate") \
+                .join(hr24_trUT_br2, "Isolate") \
+                .join(hr6_trDHA_br1, "Isolate") \
+                .join(hr6_trDHA_br2, "Isolate") \
+                .join(hr6_trUT_br1, "Isolate") \
+                .join(hr6_trUT_br2, "Isolate") \
+
+## Write to Blob
+shaped_train.coalesce(1).write.csv("/mnt/malaria/SubCh1_TrainingData_Shaped.csv")
+
+display(shaped_train)
+
+# COMMAND ----------
+
 # DBTITLE 1,Define Columns
 #Note: Columns with periods (.) in their names have been changed to underscore (_).
 
