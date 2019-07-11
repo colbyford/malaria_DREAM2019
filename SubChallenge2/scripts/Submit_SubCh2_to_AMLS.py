38dd3bcedda66fec5e856660d1b284df00e31f89
Colby Ford <colbytylerford@gmail.com>
2019-07-11 10:50:22 -0400

    Add 7-11-19 SubCh2 Model, Add 7-11-19 SubCh2 Submission Data

--- SubChallenge2/scripts/Submit_SubCh2_to_AMLS.py
+++ SubChallenge2/scripts/Submit_SubCh2_to_AMLS.py
@@ -114,7 +114,7 @@ Retrieve Best Model
 # print(best_run)
 # print(fitted_model)
 
-fitted_model = pickle.load(open("../model/amls_model_7-10-19/sc2_model.pkl","rb"))
+fitted_model = pickle.load(open("../model/amls_model_7-11-19/sc2_model.pkl","rb"))
 
 #%%
 """
